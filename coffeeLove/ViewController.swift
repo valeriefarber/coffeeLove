c35bef2cd17542771181376b15e7ede0f1c48994
Valerie Farber <valerie.farberm@gmail.com>
2018-01-23 10:14:03 -0800

    Initial Commit

---
+++ coffeeLove/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coffeeLove
+//
+//  Created by Valerie Farber on 1/23/18.
+//  Copyright © 2018 farbiedoll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
